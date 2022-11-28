ad07dd41f4c71abf99c59b8d44c1e5784274c4ff
MishOd1 <104631140+MishOd1@users.noreply.github.com>
2022-11-28 15:44:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import Graph from './Graph.js'
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <Graph />
+    </div>
+  );
+}
